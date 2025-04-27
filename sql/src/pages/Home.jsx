ae7225a5c7dc682918fa43b87b3907ce34579c43
Rao SHahbaz <menlenon59@gmail.com>
2025-04-27 21:40:20 +0500

    Home Page

--- sql/src/pages/Home.jsx
+++ sql/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <Button>
-      HELOOW HMZA
+    <div className='min-h-screen flex items-center justify-center flex-col gap-2'>
+      <Button className='p-5 pl-20 pr-20'>
+        <Link to="/login">
+          LOGIN
+        </Link>
+      </Button>
+      <Button className='p-5 pl-20 pr-20'>
+        <Link to="/signup">
+          SIGN UP
+        </Link>
       </Button>
     </div>
   )
